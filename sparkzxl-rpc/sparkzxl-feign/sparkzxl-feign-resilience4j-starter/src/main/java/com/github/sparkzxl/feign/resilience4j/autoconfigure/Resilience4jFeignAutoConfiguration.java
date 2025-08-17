b3e49139e33380372875c7b737ed5a217af40d09
zhouxinlei <zhouxinlei298@163.com>
2025-08-17 15:36:28 +0800

    代码更新

--- sparkzxl-rpc/sparkzxl-feign/sparkzxl-feign-resilience4j-starter/src/main/java/com/github/sparkzxl/feign/resilience4j/autoconfigure/Resilience4jFeignAutoConfiguration.java
+++ sparkzxl-rpc/sparkzxl-feign/sparkzxl-feign-resilience4j-starter/src/main/java/com/github/sparkzxl/feign/resilience4j/autoconfigure/Resilience4jFeignAutoConfiguration.java
@@ -8,18 +8,16 @@ import io.github.resilience4j.retry.Retry;
 import io.github.resilience4j.retry.RetryConfig;
 import io.github.resilience4j.retry.RetryRegistry;
 import io.vavr.control.Try;
-import org.springframework.beans.factory.ObjectProvider;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.core.env.Environment;
 
 import java.util.List;
-import java.util.function.Function;
 
 /**
  * description:
@@ -37,7 +35,13 @@ public class Resilience4jFeignAutoConfiguration {
     public FeignDecorators.Builder defaultBuilder(Environment environment,
                                                   RetryRegistry retryRegistry) {
         String name = environment.getProperty("feign.client.name");
-        assert name != null;
+        if (StringUtils.isEmpty(name)) {
+            RetryConfig defaultConfig = retryRegistry.getDefaultConfig();
+            Retry retry = Retry.of("defaultClient",
+                    RetryConfig.from(defaultConfig).retryOnException(throwable -> throwable instanceof feign.RetryableException)
+                            .build());
+            return FeignDecorators.builder().withRetry(retry);
+        }
         Retry retry = Try.of(() -> retryRegistry.retry(name, name)).getOrElseGet(throwable -> retryRegistry.retry(name));
         //覆盖其中的异常判断，只针对 feign.RetryableException 进行重试，所有需要重试的异常我们都在 DefaultErrorDecoder 以及 Resilience4jFeignClient 中封装成了 RetryableException
         retry = Retry.of(name,
@@ -50,21 +54,8 @@ public class Resilience4jFeignAutoConfiguration {
     @Scope("prototype")
     @ConditionalOnMissingBean
     public Feign.Builder feignResilience4jBuilder(FeignDecorators.Builder builder,
-                                                  List<RequestInterceptor> requestInterceptorList,
-                                                  // 注入 Feign 客户端对应的 fallback 实例（通过泛型匹配具体类型）
-                                                  ObjectProvider<?> fallbackObjectProvider,
-                                                  ObjectProvider<FallbackFactory<?>> factoryObjectProvider) {
-        fallbackObjectProvider.ifAvailable(fallback -> {
-            // 将 fallback 实例添加到 Resilience4j 装饰器
-            // 第二个参数：触发降级的异常类型（通常为 Exception 表示所有异常）
-            builder.withFallback(fallback, Exception.class);
-        });
-        // 3. 注册降级工厂（用于为每个 Feign 客户端提供降级实现）
-        factoryObjectProvider.ifAvailable(fallbackFactory -> {
-            Function<Exception, ?> function = fallbackFactory::create;
-            builder.withFallbackFactory(function);
-        });
-        return Resilience4jFeign.builder(builder.build())
+                                                  List<RequestInterceptor> requestInterceptorList) {
+        return Resilience4jFeign.builder(builder)
                 .requestInterceptors(requestInterceptorList)
                 .decode404();
     }
---
+++ sparkzxl-rpc/sparkzxl-feign/sparkzxl-feign-resilience4j-starter/src/main/java/io/github/resilience4j/feign/Resilience4jFeign.java
@@ -0,0 +1,129 @@
+package io.github.resilience4j.feign;
+
+import feign.Contract;
+import feign.Feign;
+import feign.InvocationHandlerFactory;
+import feign.Target;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.cloud.openfeign.FallbackFactory;
+import org.springframework.cloud.openfeign.FeignClientFactoryBean;
+import org.springframework.cloud.openfeign.FeignContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.util.StringUtils;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * description: 重写Resilience4jFeign，支持FeignClient注解的降级类
+ *
+ * @author zhouxinlei
+ * @since 2025-08-17 15:13:44
+ */
+public class Resilience4jFeign {
+
+    public static Builder builder(FeignDecorators.Builder decoratorsBuilder) {
+        return new Builder(decoratorsBuilder);
+    }
+
+    public static final class Builder extends Feign.Builder
+            implements ApplicationContextAware {
+
+        private final FeignDecorators.Builder decoratorsBuilder;
+
+        private ApplicationContext applicationContext;
+
+        private FeignContext feignContext;
+
+        public Builder(FeignDecorators.Builder decoratorsBuilder) {
+            this.decoratorsBuilder = decoratorsBuilder;
+        }
+
+        @Override
+        public Feign.Builder invocationHandlerFactory(
+                InvocationHandlerFactory invocationHandlerFactory) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Feign build() {
+            super.invocationHandlerFactory(new InvocationHandlerFactory() {
+
+                @Override
+                public InvocationHandler create(Target target,
+                                                Map<Method, MethodHandler> dispatch) {
+
+                    GenericApplicationContext gctx = (GenericApplicationContext) Builder.this.applicationContext;
+                    BeanDefinition def = gctx.getBeanDefinition(target.type().getName());
+
+                    /**
+                     * Due to the change of the initialization sequence, BeanFactory.getBean will cause a circular dependency.
+                     * So FeignClientFactoryBean can only be obtained from BeanDefinition
+                     */
+                    FeignClientFactoryBean feignClientFactoryBean = (FeignClientFactoryBean) def.getAttribute("feignClientsRegistrarFactoryBean");
+
+                    Class fallback = feignClientFactoryBean.getFallback();
+                    Class fallbackFactory = feignClientFactoryBean.getFallbackFactory();
+                    String beanName = feignClientFactoryBean.getContextId();
+
+                    if (!StringUtils.hasText(beanName)) {
+                        beanName = feignClientFactoryBean.getName();
+                    }
+
+                    Object fallbackInstance;
+                    FallbackFactory fallbackFactoryInstance;
+                    // check fallback and fallbackFactory properties
+                    if (void.class != fallback) {
+                        fallbackInstance = getFromContext(beanName, "fallback", fallback,
+                                target.type());
+                        FeignDecorators invocationDecorator = decoratorsBuilder.withFallback(fallbackInstance).build();
+                        return new DecoratorInvocationHandler(target, dispatch, invocationDecorator);
+                    }
+                    if (void.class != fallbackFactory) {
+                        fallbackFactoryInstance = (FallbackFactory) getFromContext(
+                                beanName, "fallbackFactory", fallbackFactory,
+                                FallbackFactory.class);
+                        Function<Exception, ?> function = fallbackFactoryInstance::create;
+                        FeignDecorators invocationDecorator = decoratorsBuilder.withFallbackFactory(function).build();
+                        return new DecoratorInvocationHandler(target, dispatch, invocationDecorator);
+                    }
+                    FeignDecorators invocationDecorator = decoratorsBuilder.build();
+                    return new DecoratorInvocationHandler(target, dispatch, invocationDecorator);
+                }
+
+                private Object getFromContext(String name, String type,
+                                              Class fallbackType, Class targetType) {
+                    Object fallbackInstance = feignContext.getInstance(name,
+                            fallbackType);
+                    if (fallbackInstance == null) {
+                        throw new IllegalStateException(String.format(
+                                "No %s instance of type %s found for feign client %s",
+                                type, fallbackType, name));
+                    }
+
+                    if (!targetType.isAssignableFrom(fallbackType)) {
+                        throw new IllegalStateException(String.format(
+                                "Incompatible %s instance. Fallback/fallbackFactory of type %s is not assignable to %s for feign client %s",
+                                type, fallbackType, targetType, name));
+                    }
+                    return fallbackInstance;
+                }
+            });
+            return super.build();
+        }
+
+        @Override
+        public void setApplicationContext(ApplicationContext applicationContext)
+                throws BeansException {
+            this.applicationContext = applicationContext;
+            feignContext = this.applicationContext.getBean(FeignContext.class);
+        }
+
+    }
+
+}
