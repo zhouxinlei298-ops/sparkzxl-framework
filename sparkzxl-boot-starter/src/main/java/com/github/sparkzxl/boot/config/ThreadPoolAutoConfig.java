ea238237ae8f717b81dd226ca8369f01cb471f7d
zhouxinlei <zhouxinlei298@163.com>
2025-09-09 09:26:33 +0800

    代码更新

--- sparkzxl-boot-starter/src/main/java/com/github/sparkzxl/boot/config/ThreadPoolAutoConfig.java
+++ sparkzxl-boot-starter/src/main/java/com/github/sparkzxl/boot/config/ThreadPoolAutoConfig.java
@@ -1,18 +1,13 @@
 package com.github.sparkzxl.boot.config;
 
-import static org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME;
 
 import com.alibaba.ttl.threadpool.TtlExecutors;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
 import org.springframework.boot.autoconfigure.task.TaskExecutionProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.scheduling.annotation.AsyncAnnotationBeanPostProcessor;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
@@ -24,17 +19,15 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
  */
 @EnableAsync
 @Configuration
-@AutoConfigureBefore(value = TaskExecutionAutoConfiguration.class)
 @Slf4j
 public class ThreadPoolAutoConfig {
 
     public ThreadPoolAutoConfig() {
-        log.info("start init taskExecutor");
+        log.info("start init ttl taskExecutor");
     }
 
-    @Bean(name = {APPLICATION_TASK_EXECUTOR_BEAN_NAME, AsyncAnnotationBeanPostProcessor.DEFAULT_TASK_EXECUTOR_BEAN_NAME})
-    @Primary
-    public Executor taskExecutor(TaskExecutionProperties taskExecutionProperties) {
+    @Bean(name = "ttlTaskExecutor")
+    public Executor ttlTaskExecutor(TaskExecutionProperties taskExecutionProperties) {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         TaskExecutionProperties.Pool pool = taskExecutionProperties.getPool();
         //核心线程池大小
@@ -46,13 +39,16 @@ public class ThreadPoolAutoConfig {
         //活跃时间
         executor.setKeepAliveSeconds((int) pool.getKeepAlive().getSeconds());
         //线程名字前缀
-        executor.setThreadNamePrefix(taskExecutionProperties.getThreadNamePrefix());
+        executor.setThreadNamePrefix("ttl-task-executor-");
         TaskExecutionProperties.Shutdown shutdown = taskExecutionProperties.getShutdown();
+
         // 设置线程池关闭的时候等待所有任务都完成再继续销毁其他的Bean
         executor.setWaitForTasksToCompleteOnShutdown(shutdown.isAwaitTermination());
+        // 设置这个执行器在关闭时应该阻止的最大秒数
+        executor.setAwaitTerminationSeconds(30);
         // 线程池对拒绝任务的处理策略,当线程池没有处理能力的时候，该策略会直接在 execute 方法的调用线程中运行被拒绝的任务；如果执行程序已关闭，则会丢弃该任务
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         executor.initialize();
-        return TtlExecutors.getTtlExecutor(executor);
+        return TtlExecutors.getTtlExecutorService(executor.getThreadPoolExecutor());
     }
 }
