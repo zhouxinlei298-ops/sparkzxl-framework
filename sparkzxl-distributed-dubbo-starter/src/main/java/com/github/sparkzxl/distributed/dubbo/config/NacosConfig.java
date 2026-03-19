8839cf29297db91f7fdcfef0f9792e6d21c2f0a1
zhouxinlei <zhouxinlei298@163.com>
2026-03-19 17:28:10 +0800

    dubbo-starter更改

---
+++ sparkzxl-distributed-dubbo-starter/src/main/java/com/github/sparkzxl/distributed/dubbo/config/NacosConfig.java
@@ -0,0 +1,31 @@
+package com.github.sparkzxl.distributed.dubbo.config;
+
+import cn.hutool.core.date.DateUtil;
+import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
+import com.alibaba.cloud.nacos.NacosServiceManager;
+import com.alibaba.cloud.nacos.discovery.NacosWatch;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+/**
+ * description:
+ *
+ * @author zhouxinlei
+ * @since 2022-03-19 23:02:53
+ */
+@Configuration
+public class NacosConfig {
+
+    @Bean
+    @ConditionalOnProperty(value = {"spring.cloud.nacos.discovery.watch.enabled"}, matchIfMissing = true)
+    public NacosWatch nacosWatch(NacosServiceManager nacosServiceManager, NacosDiscoveryProperties nacosDiscoveryProperties,
+            ObjectProvider<ThreadPoolTaskScheduler> taskScheduler) {
+        //更改服务详情中的元数据，增加服务注册时间
+        nacosDiscoveryProperties.getMetadata().put("startup.time", DateUtil.now());
+        return new NacosWatch(nacosServiceManager, nacosDiscoveryProperties, taskScheduler);
+    }
+
+}
